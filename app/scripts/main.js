3d331be46808cc619d3c8b3ad3027b3c66b2c94c
mariana-martins <mariana_martins@id.uff.br>
2017-08-03 15:59:11 +1200

    Implement scrollspy and fix anchors link problem

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,33 +9,14 @@ $(function() {
 
 /* Navbar change color during the scroll */
 
-$(document).ready(function(){
-  var scroll_start = 0;
-  var startchange = $('#about');
-  var offset = startchange.offset();
-  if (startchange.length){
-    $(document).scroll(function() {
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-        // ;
-        // $("#navbar-site").css('height', '20px');
-        $('#navbar-site').addClass(".teste");
-      } else {
-
-      }
-    });
-  }
-});
-
-
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-    $(".fixed-top").addClass("top-nav-collapse");
-    $("#navbar-site").css('background-color', 'rgba(154, 23, 77, 0.85)');
+  var carlinhos = $('#navbar-site');
+  if ($('.navbar').offset().top > 50) {
+    $('.fixed-top').addClass('top-nav-collapse');
+    $('#navbar-site').css('background-color', 'rgba(154, 23, 77, 0.75)');
   } else {
-    $(".fixed-top").removeClass("top-nav-collapse");
-    $('#navbar-site').css('background-color', 'transparent');
+    $('.fixed-top').removeClass('top-nav-collapse');
+    carlinhos.css('background-color', 'transparent');
+    carlinhos.removeClass('active');
   }
 });
-
-
