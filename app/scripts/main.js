6b434c8a8c04b29fc123431ea85c249163a1487f
mariana-martins <mariana_martins@id.uff.br>
2017-08-03 14:31:14 +1200

    Animate the navbar during the scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,41 @@
-$('#container').sliphover();
+/* Config project image hover */
+$(function() {
+
+  $('#container').sliphover({
+    backgroundColorAttr: 'data-background'
+  });
+
+});
+
+/* Navbar change color during the scroll */
+
+$(document).ready(function(){
+  var scroll_start = 0;
+  var startchange = $('#about');
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+        // ;
+        // $("#navbar-site").css('height', '20px');
+        $('#navbar-site').addClass(".teste");
+      } else {
+
+      }
+    });
+  }
+});
+
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+    $(".fixed-top").addClass("top-nav-collapse");
+    $("#navbar-site").css('background-color', 'rgba(154, 23, 77, 0.85)');
+  } else {
+    $(".fixed-top").removeClass("top-nav-collapse");
+    $('#navbar-site').css('background-color', 'transparent');
+  }
+});
+
+
