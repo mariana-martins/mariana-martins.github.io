4c20aa0e46615f79f034ac8dc25bcac3cc841d6b
mariana-martins <mariana_martins@id.uff.br>
2017-09-17 21:15:46 +1200

    Add interests template

--- app/index.js
+++ app/index.js
@@ -17,12 +17,17 @@ var aboutComponent = require('./templates/about');
 var projectsComponent = require('./templates/projects');
 var educationComponent = require('./templates/education');
 var questionsComponent = require('./templates/questions');
+var interestsComponent = require('./templates/interests');
 
 // data
 var profileData = require('./data/profile.json');
 var projectsData = require('./data/projects.json');
 var educationData = require('./data/education.json');
 var questionsData = require('./data/questions.json');
+var interestsData = {
+  articles: require('./data/interests/articles.json'),
+  books: require('./data/interests/books.json')
+};
 
 
 // functions
@@ -37,6 +42,7 @@ loadTemplate('about', profileData, aboutComponent);
 loadTemplate('projects', projectsData, projectsComponent);
 loadTemplate('education', {"education": educationData, "profile": profileData}, educationComponent);
 loadTemplate('questions', questionsData, questionsComponent);
+loadTemplate('interests', interestsData, interestsComponent);
 
 // js dependencies
 require('bootstrap');
