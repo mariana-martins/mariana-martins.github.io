e48f257bc305eeaecbcfd04b28b94da858acd840
mariana-martins <mariana_martins@id.uff.br>
2017-09-24 19:35:16 +1300

    Fix navbar on mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,35 @@
 /* Change color of Navbar, during the scroll */
-$(window).scroll(function() {
-  var navbar = $('#navbar-site');
-  if ($('.navbar').offset().top > 50) {
-    $('.fixed-top').addClass('top-nav-collapse');
-    $('body').addClass('top-page');
-  } else {
-    $('.fixed-top').removeClass('top-nav-collapse');
-    navbar.removeClass('active');
-    $('body').removeClass('top-page');
+
+// The following code is a workaround to work on tablets and mobile.
+// jQuery.scroll wasn't working reliably.
+// As I want to update navbar color based on its position, I decided
+// to keep track of it and validate it every 100ms, using setInterval.
+// Still, it's only activated after scroll animation ends on mobile.
+// To remove workaround:
+//  * comment line 31, about setInteval function
+//  * uncomment line 32, about using jQuery and scroll function
+var updateNavBarColor = function() {
+  var usingTopPage = $('.navbar').offset().top > 50;
+
+  return function() {
+    var top50pixels = $('.navbar').offset().top > 50;
+
+    if (top50pixels) {
+      if (!usingTopPage) {
+        $('.fixed-top').addClass('top-nav-collapse');
+        $('body').addClass('top-page');
+        usingTopPage = true;
+      }
+    } else if (usingTopPage) {
+      $('.fixed-top').removeClass('top-nav-collapse');
+      $('#navbar-site').removeClass('active');
+      $('body').removeClass('top-page');
+      usingTopPage = false;
+    }
   }
-});
+}();
+setInterval(updateNavBarColor, 100);
+// $(window).scroll(updateNavBarColor);
 
 // Call to Github Api using Github calendar
 var GitHubCalendar = require('github-calendar');
