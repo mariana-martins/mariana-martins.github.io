a74199ba0fceeea9b9528b88f82dc9237faa6928
mariana-martins <mariana.menezes@kathmandu.co.nz>
2019-07-31 14:22:16 +1200

    Updated content

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,13 +49,7 @@ class Index extends React.Component {
                   <h2>About Me</h2>
                 </header>
                 <p>
-                  Right now I’m mainly focusing on Front-End User Experience
-                  technologies. Devotion and responsibility are only some
-                  characteristics of my personality and I'm looking for
-                  opportunities to share my knowledge and to show my work. I
-                  believe in an easily accessible Web for everybody and in the
-                  future I would like to be part of the creation of an inclusive
-                  web using my Front-end knowledge.
+                  Hi! My name is Mariana. I live in a nice city, called Christchurch! I'm a big fan of Lord of the Rings! What other reason is there to live in New Zealand? I'm joking, this country is really beautiful and I love living here. My hobbies include reading adventure and dystopian books, building something using Lego, watching some NFL game and listening to music. I can't live without music. Devotion and responsibility are some characteristics of my personality. I believe in an easily accessible Web for everybody and in the future I would like to be part of the creation of an inclusive web using my Front-end knowledge.
                 </p>
               </div>
               <span className="image">
@@ -70,27 +64,24 @@ class Index extends React.Component {
             </header>
             <ul className="features">
               <li>
-                <span className="icon major style1 fas fa-store" />
-                <h3>Kathmandu Holdings Website</h3>
+                <span className="icon major style1 fa fa-map-signs" />
+                <h3>Neighborhood Map App</h3>
                 <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
+                  I ​developed a SPA that loads a list of restaurants located in Christchurch on a map. I used Zomato API to get the restaurant list and Google Maps API to visualize them on a map as markers.
                 </p>
               </li>
               <li>
                 <span className="icon major style3 fa fa-paw" />
                 <h3>Husky Rescue Org. Website</h3>
                 <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
+                  It was SPA using React, Bootstrap 4, Sass and Webpack. I also developed the new website mockups using Photoshop. It was a proposing to replace the old Husky Rescue website.
                 </p>
               </li>
               <li>
-                <span className="icon major style5 fa fa-map-signs" />
-                <h3>Neighborhood Map App</h3>
+                <span className="icon major style5 fas fa-lock" />
+                <h3>Projects at Kathmandu</h3>
                 <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
+                  I'm currently working in internal projects using React, Gatsby.js and Netlify at Kathmandu. The main project is the Kathmandu Ecommerce website which use Magento 2.2.9, Knockout.js and Less.
                 </p>
               </li>
             </ul>
@@ -104,7 +95,25 @@ class Index extends React.Component {
               </ul>
             </footer>
           </section>
-          {/* TODO: Escrever texto sobre educação */}
+
+          <section id="cta" className="main special">
+            <header className="major">
+              <h2>Experience</h2>
+              <div style={{ textAlign: 'left' }}>
+                <h3><b><i class="fas fa-hashtag"></i> Kathmandu as Junior Frontend Developer</b></h3>
+                <h4>October/2018 - On going</h4>
+                <p>
+                  As a member of the Web Development team, my main role is to code highly efficient and scalable software to maintain, extend and enhance Kathmandu’s eCommerce website. I am currently working with HTML5, CSS3, Javascript and Magento 2. I’m also responsible for code review and development of tags on Google Tag Manager. One of my recent achievements is a website prototype, that is planned to released after finalising more features. Its main technologies are React, GraphQL, Gatsby.js, and Netlify CMS.
+                </p>
+                <h3><b><i class="fas fa-hashtag"></i> Chingu Cohorts Project as Frontend Developer</b></h3>
+                <h4>December/2017 - January/2018</h4>
+                <p>
+                  My main role was Frontend developer but also acted as Project Manager. As a Frontend Developer, I designed and implemented the Husky Rescue webpage. Its design was created using Adobe Photoshop. It was developed using React, Bootstrap 4, SASS and Webpack.
+                </p>
+              </div>
+            </header>
+          </section>
+
           <section id="second" className="main special">
             <header className="major">
               <h2>Skills & Education</h2>
@@ -136,62 +145,11 @@ class Index extends React.Component {
               </li>
             </ul>
             <p className="content">
-              Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl
-              eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum
-              ac arcu sit amet, fermentum pellentesque et purus. Integer maximus
-              varius lorem, sed convallis diam accumsan sed. Etiam porttitor
-              placerat sapien, sed eleifend a enim pulvinar faucibus semper quis
-              ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer
-              eget purus nec nulla mattis et accumsan ut magna libero. Morbi
-              auctor iaculis porttitor. Sed ut magna ac risus et hendrerit
-              scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras
-              eu ornare dui curabitur lacinia.
+              I've studied History at the Fluminense Federal University, in Rio. However, during my bachelor, I realized that the impact of my work in society didn't satisfy me. So, I decided to try something completely different and I have no regrets about it. After finishing my bachelor degree, I started studying basics Web development subjects. Then, I discovered that my passion for Frontend development could be my profession in the future. Although I still enjoy reading about History, currently I am in love with the frontend development. In 2019, I started my studies about React when I participated in Chingu Cohorts journey. After that, I'm currently learning Gatsby while developing some internal projects for Kathmandu. I've also developed skills to use Sass, Bootstrap 3 & 4, GraphQL and Webpack. My current interests are more about UX and accessibility.
             </p>
           </section>
 
-          <section id="cta" className="main special">
-            <header className="major">
-              <h2>Experience</h2>
-              <div style={{ textAlign: 'left' }}>
-                <h3><b>Kathmandu as Frontend Developer</b></h3>
-                <p>
-                  Cep risus aliquam gravida cep ut lacus amet. Adipiscing
-                  faucibus nunc placerat. Tempus adipiscing turpis non blandit
-                  accumsan eget lacinia nunc integer interdum amet aliquam ut
-                  orci non col ut ut praesent. Semper amet interdum mi.
-                  Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae
-                  ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat
-                  gravida placerat adipiscing in risus fusce vitae ac mi
-                  accumsan nunc in accumsan tempor blandit aliquet aliquet
-                  lobortis. Ultricies blandit lobortis praesent turpis.
-                  Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci
-                  blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio
-                  vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id.
-                  Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam.
-                  Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum
-                  condimentum semper commodo massa arcu.
-                </p>
-                <h3><b>Chingu Cohorts as Frontend Developer</b></h3>
-                <p>
-                  Cep risus aliquam gravida cep ut lacus amet. Adipiscing
-                  faucibus nunc placerat. Tempus adipiscing turpis non blandit
-                  accumsan eget lacinia nunc integer interdum amet aliquam ut
-                  orci non col ut ut praesent. Semper amet interdum mi.
-                  Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae
-                  ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat
-                  gravida placerat adipiscing in risus fusce vitae ac mi
-                  accumsan nunc in accumsan tempor blandit aliquet aliquet
-                  lobortis. Ultricies blandit lobortis praesent turpis.
-                  Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci
-                  blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio
-                  vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id.
-                  Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam.
-                  Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum
-                  condimentum semper commodo massa arcu.
-                </p>
-              </div>
-            </header>
-          </section>
+
         </div>
       </Layout>
     )
