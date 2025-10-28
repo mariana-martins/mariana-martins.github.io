9a7d99ef52d0fb6c7cfd0b3606bd1416b0708cba
mariana-martins <marianamartinsmenezes@gmail.com>
2025-10-28 16:40:25 +1300

    Remove unused cn utility and bugs field from package.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import "./styles/index.css";
 
 function App() {
   return (
-    // <div className="min-h-screen bg-background text-foreground">
-    //   <main className="container mx-auto px-4 py-8">
-    //     <h1 className="text-4xl font-bold text-center mb-8">
-
-    //     </h1>
-
-    //   </main>
-    // </div>
-
-    <div className="min-h-screen grid grid-cols-2 grid-rows-3 gap-4">
+    <div className="min-h-screen grid grid-cols-2 grid-rows-3 gap-4 px-16">
       <header className="col-span-2">
         <h1 className="text-4xl font-bold font-heading tracking-[0.25rem] text-heading">
           Welcome to Mariana Martins Portfolio
